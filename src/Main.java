129695b3a7abdd2d706503944ad282d833ac9706
TheJuliana <juliana.svetlakova@gmail.com>
2023-09-12 06:28:00 +0200

    lab2 done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void lab1() {
         System.out.printf("------Laboratory work №1------ \n");
@@ -22,7 +24,33 @@ public class Main {
         System.out.printf("Output number:  " + number + "\n");
         System.out.printf("Count of steps: " + countOfSteps + "\n");
     }
+
+    public static void  lab2() {
+        System.out.printf("------Laboratory work №2------ \n");
+        System.out.printf("Enter the size of array: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        ArrayList<Integer> numbers = new ArrayList<>();
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Enter the number №" + (i+1) + ": ");
+            numbers.add(s.nextInt());
+        }
+        for (int i = 0; i < n; i++) {
+            if ((i+1)%2 != 0) {
+                result += numbers.get(i);
+            } else {
+                result -= numbers.get(i);
+            }
+        }
+
+        System.out.printf("Input number: " + n + "\n");
+        for (int i = 0; i < n; i++) {
+            System.out.printf("i№" + i + ": " + numbers.get(i) + "\n");
+        }
+        System.out.printf("Result number: " + result + "\n");
+    }
     public static void main(String[] args) {
-        lab1();
+        lab2();
     }
 }

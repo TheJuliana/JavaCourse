005b5523bb0cad79572b1edb05a3b064b93dc7e5
TheJuliana <juliana.svetlakova@gmail.com>
2023-10-06 16:12:38 +0200

    some changes

--- src/Lab3/src/Cinema.java
+++ src/Lab3/src/Cinema.java
@@ -1,32 +1,54 @@
 package Lab3.src;
 
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class Cinema {
-    private ArrayList<Hall> _halls = new ArrayList<Hall>();
-    public static void createHall(){
-        Scanner s = new Scanner(System.in);
+    private ArrayList<Hall> halls = new ArrayList<Hall>();
+/*    public void addHall(Hall hall) {
+        halls.add(hall);
+    }*/
+public Cinema() {};
+    public void createHall(){
         System.out.println("------Welcome to the creation of halls------");
-        System.out.println("Enter the width of the row and the number of seats and movies ");
-        int rows = s.nextInt();
-        int columns = s.nextInt();
-        System.out.println("rows "+rows+" cols "+columns);
-        ArrayList<ArrayList<Boolean>> seats = new ArrayList<ArrayList<Boolean>>();
-        for (int i = 0; i < rows; i++) {
-            ArrayList<Boolean> row = new ArrayList<>(columns);
-            for (int j = 0; j < columns; j++) {
-                row.add(Boolean.TRUE);
+        CREATION:
+        while (true) {
+            Scanner s = new Scanner(System.in);
+            System.out.println("Enter the width of the row and the number of seats and movies ");
+            int rows = s.nextInt();
+            int columns = s.nextInt();
+            System.out.println("rows " + rows + " cols " + columns);
+            if (rows == 0 && columns == 0) {
+                System.out.println("Wrong");
+                continue CREATION;
+            } else {
+                ArrayList<ArrayList<Boolean>> seats = new ArrayList<ArrayList<Boolean>>();
+                ArrayList<ArrayList<Movie>> movies = new ArrayList<ArrayList<Movie>>();
+                for (int i = 0; i < rows; i++) {
+                    ArrayList<Boolean> row = new ArrayList<>(columns);
+                    for (int j = 0; j < columns; j++) {
+                        row.add(j, Boolean.TRUE);
+                    }
+                    seats.add(row);
+                }
+                System.out.println("seats sze = " + seats.size());
+                System.out.println(seats);
+
+                Hall hall = new Hall(seats, movies);
+                this.halls.add(hall);
+                System.out.println("------Created------");
+                System.out.println("Want to add another hall? (TYPE: Y if yes)");
+                String answer;
+                Scanner s2 = new Scanner(System.in);
+                answer = s2.nextLine();
+                if (Objects.equals(answer, "Y") || Objects.equals(answer, "y")) {
+                    continue CREATION;
+                } else {
+                    break CREATION;
+                }
             }
-            seats.add(row);
         }
-   /*     System.out.println("seats sze =" + seats.size());
-        for (int i = 0; i < seats.size(); i++) {
-            for (int j = 0; j < seats.get(i).size(); j++){
-                //System.out.println(seats);
-                System.out.println("1");
-            }
-        }*/
-        System.out.println("------Created------");
-    };
+    }
 }
--- src/Lab3/src/Hall.java
+++ src/Lab3/src/Hall.java
@@ -5,99 +5,10 @@ import java.util.*;
 public class Hall {
     private ArrayList<ArrayList<Boolean>> seats = new ArrayList<ArrayList<Boolean>>(); //кресла (занято/не занято)
     //private ArrayList<Movie> movieSessions = new ArrayList<Movie>();
-    private Queue<Movie> movies = new Queue<Movie>() {
-        @Override
-        public boolean add(Movie movie) {
-            return false;
-        }
-
-        @Override
-        public boolean offer(Movie movie) {
-            return false;
-        }
-
-        @Override
-        public Movie remove() {
-            return null;
-        }
-
-        @Override
-        public Movie poll() {
-            return null;
-        }
-
-        @Override
-        public Movie element() {
-            return null;
-        }
-
-        @Override
-        public Movie peek() {
-            return null;
-        }
-
-        @Override
-        public int size() {
-            return 0;
-        }
-
-        @Override
-        public boolean isEmpty() {
-            return false;
-        }
-
-        @Override
-        public boolean contains(Object o) {
-            return false;
-        }
-
-        @Override
-        public Iterator<Movie> iterator() {
-            return null;
-        }
-
-        @Override
-        public Object[] toArray() {
-            return new Object[0];
-        }
-
-        @Override
-        public <T> T[] toArray(T[] a) {
-            return null;
-        }
-
-        @Override
-        public boolean remove(Object o) {
-            return false;
-        }
-
-        @Override
-        public boolean containsAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean addAll(Collection<? extends Movie> c) {
-            return false;
-        }
-
-        @Override
-        public boolean removeAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public boolean retainAll(Collection<?> c) {
-            return false;
-        }
-
-        @Override
-        public void clear() {
-
-        }
-    };
-    Hall() {
-
+    private ArrayList<ArrayList<Movie>> movies = new ArrayList<ArrayList<Movie>>();
+    Hall(ArrayList<ArrayList<Boolean>> seats, ArrayList<ArrayList<Movie>>  movies) {
+        this.seats = seats;
+        this.movies = movies;
     }
     public static void createMovie(){
         System.out.println("------Movie Creation-------");
